26d1464b4c222a70f9fa0dfb994fdb9b97910975
NouranHany <hany.noran@yahoo.com>
2021-10-14 23:23:24 +0200

    Adds the new item functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+
+//requiring needed modules
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const localPort = 3000;
+let items = [];
+//creating an express server application
+const app = express();
+app.use(express.static("public")); //serving static files via the server
+app.use(bodyParser.urlencoded({extended:true})); //help parsing html forms via bodyParser
+app.set('view engine','ejs');
+
+//get the home page
+app.get("/",function(req,res){
+  console.log(items);
+  res.render("index",{items:items});
+});
+
+//add new item to the home page
+app.post("/",function(req,res){
+  items.push(req.body.newItem);
+  res.redirect("/");
+});
+
+//attaching server to listen on local port 3000
+app.listen(localPort,function(){
+  console.log("Server starting on port: " + localPort);
+})
